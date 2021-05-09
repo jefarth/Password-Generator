04bf88d432c0a0ed4cb72e4a25709b0456c021f7
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-05-09 17:25:34 -0600

    Wrote psuedo-code to make a framework

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,14 +1,45 @@
+// Create prompts for password criteria
+// when button is clicked
+
+// Choose password length bteween 8-128 char
+
+// Choose character types to include in password
+// lowercase, uppercase, numeric, and/or special
+
+// Take prompt inputs and generate appropriate password in textarea
+
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector(`#generate`);
+const lowerCase = `abcdefghijklmnopqrstuvwxyz`;
+const upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+const number = `0123456789`;
+const special = `!@#$%^&*()_-+={}[];:'"~<,>.?/|`;
+const passwordLength;
+const uppercaseCheck;
+const numberCheck;
+const specialCheck;
+
+// Create function to determine length of password
+
+// Create function to ask if user wants to use lowercase letters
+
+// Create function to ask if user wants to use uppercase letters
+
+// Create function to ask if user wants to use numbers
+
+// Create function to ask if user wants to use special char
+
+// Create function to compile previous answers and generate random password
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
