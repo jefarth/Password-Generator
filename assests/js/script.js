d804b48e72ab7072285ab80abd97eda75e4fb71a
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-05-09 19:17:35 -0600

    Created prompt to set length & added more framework

--- assests/js/script.js
+++ assests/js/script.js
@@ -17,21 +17,68 @@ const upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 const number = `0123456789`;
 const special = `!@#$%^&*()_-+={}[];:'"~<,>.?/|`;
 var passwordLength;
-var uppercaseCheck;
+var upperCheck;
 var numberCheck;
 var specialCheck;
 
-// Create function to determine length of password
 
-// Create function to ask if user wants to use lowercase letters
+
+// Create function to create a prompt to check the desired length of the password
+function checkLength() {
+  passwordLength = prompt(`Please choose how many characters you would like your password to be (between 8 and 128 characters): `);
+
+// Checks if the password is less than 8 characters and alerts user if true, then re-runs the function
+  if (passwordLength < 8) {
+    alert(`Password must be at least 8 characters long`);
+    checkLength();
+    // Checks if the password is more than 128 characters and alers user if true, then re-runs the function
+  } else if (passwordLength > 128) {
+    alert(`Password must be less than 128 characters long`);
+    checkLength();
+    // Checks if the user input a character other than a number and alerts user if true, then re-runs the function
+  } else if (isNaN(passwordLength)) {
+    alert(`Must use a number to determine character length between 8 and 128 characters`);
+    checkLength();
+    // If all criteria met, set the password length to user preference and end the function
+  } else {
+    alert(`Password length set. The following three screens will allow you to choose if you want to include special characters.`);
+  }
+  // Call the completed function
+  return passwordLength;
+}
+
 
 // Create function to ask if user wants to use uppercase letters
+function checkUppercase() {
+ 
+}
+
 
 // Create function to ask if user wants to use numbers
+function checkNumber() {
+  
+}
+
 
 // Create function to ask if user wants to use special char
+function checkSpecial() {
+
+}
+
 
 // Create function to compile previous answers and generate random password
+function generatePassword() {
+  checkLength();
+  console.log(passwordLength);
+  checkUppercase();
+  console.log(upperCheck)
+  checkNumber();
+  console.log(numberCheck);
+  checkSpecial();
+  console.log(specialCheck);
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
